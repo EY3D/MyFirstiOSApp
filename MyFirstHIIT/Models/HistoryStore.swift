f5d51abc9bc91ae2e19f20fdd8eabe54f6c2588c
EY3D <92963290+EY3D@users.noreply.github.com>
2024-12-15 18:38:26 +0800

    fixed bug where exercises are always added as a new date entry.

--- MyFirstHIIT/Models/HistoryStore.swift
+++ MyFirstHIIT/Models/HistoryStore.swift
@@ -26,13 +26,18 @@ class HistoryStore: ObservableObject {
     
     func addDoneExercise(_ exerciseName: String){
         let today = Date()
+        
+        print("today is \(today)")
+        
         if exerciseDays.isEmpty {
             print("HistoryStore.exerciseDays empty, adding \(exerciseName)")
             exerciseDays.append(ExerciseDay(date: today, exercises:[exerciseName]))
-        } else if today == exerciseDays[0].date {
+        } else if Calendar.current.isDate(today, inSameDayAs: exerciseDays[0].date) {
+            print("exerciseDays[0].date is \(exerciseDays[0].date) which is a same date as \(today)")
             print("Adding \(exerciseName) to today")
             exerciseDays[0].exercises.append(exerciseName)
         } else {
+            print("exerciseDays[0].date is \(exerciseDays[0].date) which is a different date from \(today)")
             exerciseDays.insert(ExerciseDay(date: today, exercises:[exerciseName]), at: 0)
         }
     }
